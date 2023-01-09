a343fe9b9cb4147c8808c3ce5869f2789058c15c
Sebastián Gines <sebastian.gines@mercadolibre.com>
2023-01-09 18:09:48 -0300

    Agregado de modularación e interfaces

---
+++ src/06-Structuras-interfaces/04-interfaces/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type Animal interface {
+	mover() string
+}
+
+type Perro struct{}
+type Pez struct{}
+type Pajaro struct{}
+
+func (*Perro) mover() string {
+	return "Soy perro y camino"
+}
+func (*Pez) mover() string {
+	return "Soy pez y nado"
+}
+func (*Pajaro) mover() string {
+	return "Soy pájaro y vuelo"
+}
+
+func moverAnimal(animal Animal) {
+	fmt.Println(animal.mover())
+}
+
+func main() {
+	perro := Perro{}
+	perro.mover()
+	/*
+		moverAnimal(&perro)
+		pez := Pez{}
+		moverAnimal(&pez)
+		pajaro := Pajaro{}
+		moverAnimal(&pajaro)
+	*/
+}
